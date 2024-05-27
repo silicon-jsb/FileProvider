eb8dfe3d2101af60bbf25f8c61e66c237f0dad78
Johlundgren <gunbald@live.se>
2024-05-27 12:55:02 +0200

    Ändrat DataContext

--- Data/Contexts/DataContextFactory.cs
+++ Data/Contexts/DataContextFactory.cs
@@ -9,7 +9,7 @@ public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
     public DataContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-        optionsBuilder.UseSqlServer("Server=tcp:silicon-jsb.database.windows.net,1433;Initial Catalog=silicon-jsb;Persist Security Info=False;User ID=SqlAdmin;Password=Bytmig123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        optionsBuilder.UseSqlServer("Server=tcp:sqlserver-silicon-jsb.database.windows.net,1433;Initial Catalog=user-webapp-newsilicon-jsb;Persist Security Info=False;User ID=SqlAdmin;Password=Bytmig123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
         return new DataContext(optionsBuilder.Options);
     }
